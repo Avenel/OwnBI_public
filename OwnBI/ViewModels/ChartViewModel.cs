3c977aed3bad028b51266c74b4940cc59a947732
Martin Briewig <martin.briewig@gmail.com>
2016-06-06 00:13:28 +0200

    added chartjs and implemented chart partial and added 2 examples

---
+++ OwnBI/ViewModels/ChartViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OwnBI.ViewModels
+{
+    public class ChartViewModel
+    {
+        public List<float> Values { get; set; }
+        public List<string> Labels { get; set; }
+        public string Type{ get; set; }
+        public string Title { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        private string _chartID = Guid.NewGuid().ToString();
+        public string ChartID { get { return _chartID; } }
+    }
+}
